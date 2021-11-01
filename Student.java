94dd5c69fe43a45b33631345703a98702c2522e4
Ryan Kelley <kelleyr@duvalschools.org>
2021-11-01 09:23:57 -0400

    Changed Class Names to Student

---
+++ Student.java
@@ -0,0 +1,48 @@
+// Unit 2, Topic 2.1, Classes and Objects, 11/01/21, version 0.2
+
+
+public class Student {
+    // These are INSTANCE VARIABLES.  
+    private int studentID; 
+    private int gradeLevel; 
+    private double gradePointAvg; 
+    private String studentName; 
+
+
+    // Creating a Constructor 
+    public Student() {
+        this.studentID = 0;
+        this.gradeLevel = 0;
+        this.gradePointAvg = 0.0;
+        this.studentName = "";
+    }
+
+    // Value Constructor 
+    public Student(int studentID, int gradeLevel, double GPA, String studentName) {
+        this.studentID = studentID; 
+        this.gradeLevel = gradeLevel;
+        this.gradePointAvg = GPA;
+        this.studentName = studentName;
+    }
+
+    public Student(int studentID, String studentName) {
+        this.studentID = studentID;
+        this.studentName = studentName;
+        this.gradeLevel = 9;
+        this.gradePointAvg = 4.0; 
+    
+    // More than one constructor is known as OVERLOADING. 
+    
+    }
+
+
+
+    public static void main(String[] args) {
+        Student student0 = new Student(); 
+        Student student1 = new Student(12345678, 11, 0.57, "Dr. Strange");
+
+    }
+
+
+
+}
